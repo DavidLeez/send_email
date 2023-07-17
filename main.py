2184afe3e68f675ce950bf9d1b3add2643ef49f0
Dai Le <dailv.cntt@gmail.com>
2023-07-17 23:20:06 +0700

    add limitation, subject, dynamic api_key

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import requests
 from send_email import send_email
 
 api_key = "4e7d8efb067d4180a83b0d0863b0224b"
-
-url = "https://newsapi.org/v2/everything?q=tesla&" \
-      "from=2023-06-16&sortBy=publishedAt&apiKey=" \
-      "4e7d8efb067d4180a83b0d0863b0224b"
+url = "https://newsapi.org/v2/top-headlines?" \
+      "sources=techcrunch&" \
+      f"apiKey={api_key}&" \
+      "language=en"
 # make request
 request = requests.get(url)
 
@@ -14,13 +14,15 @@ content = request.json()
 
 # access the article title and description
 body = ""
-for article in content["articles"]:
-    # good solution
-    body = body + f"{article['title']}\n{article['description']}\n\n"
-
-    # original
-    # if article["title"] is not None:
-    # body = body + article["title"] + "\n" + article["description"] + 2 * "\n"
-
+# if "articles" in content:
+for article in content["articles"][:10]:
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" \
+               + article["description"] \
+               + "\n" + article["url"] + 2 * "\n"
+# for article in content["articles"][:10]:
+#     body = "Subject: Today's news" \
+#            + "\n" + body + f"{article['title']}\n{article['description']}\n{article['url']}\n\n"
+body = "Subject: Today's news" + "\n" + body
 body = body.encode("utf-8")
 send_email(message=body)
